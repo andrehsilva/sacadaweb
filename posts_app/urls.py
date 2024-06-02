ab738055f88a0ad5b73ec4eaaa938981d499a2eb
Your Name <andrehrsilva@gmail.com>
2024-06-02 14:53:50 -0300

    detail e post-list correção

--- posts_app/urls.py
+++ posts_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from .views import ListPosts, PostDetailView
+
 
 urlpatterns = [
-    path('', views.ListPosts.as_view(), name='post-list'),
+    path('', ListPosts.as_view(), name='post-list'),
+    path('post/<uuid:pk>/', PostDetailView.as_view(), name='post-detail'),
 ]
--- posts_app/views.py
+++ posts_app/views.py
@@ -1,26 +1,40 @@
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 from django.core.paginator import Paginator
 from django.shortcuts import render
 from posts_app.models import Post, Category
 from django.urls import reverse_lazy
+from django.db.models import Q
 
 
 class ListPosts(ListView):
     template_name = 'post-list.html'
     model = Post
     context_object_name = 'posts'
-    paginate_by = 2
+    paginate_by = 4
 
     def get_queryset(self):
-        # Aqui você pode personalizar sua queryset conforme necessário
-        return Post.objects.all()
-    
+        queryset = super().get_queryset()
+        search_query = self.request.GET.get('search_query', '')
+
+        if search_query:
+            queryset = queryset.filter(
+                Q(name__icontains=search_query) |
+                Q(content__icontains=search_query) |
+                Q(author__username__icontains=search_query)
+            )
+
+        return queryset
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        objetos = context['posts']
-        paginator = Paginator(objetos, self.paginate_by)
-        page_number = self.request.GET.get('page')
-        page_obj = paginator.get_page(page_number)
-        context['page_obj'] = page_obj
         context['categories'] = Category.objects.all()
+        context['search_query'] = self.request.GET.get('search_query', '')
+        context['posts_latest'] = Post.objects.order_by('-created')[:5]
         return context
+
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post-detail.html'
+    context_object_name = 'post'
